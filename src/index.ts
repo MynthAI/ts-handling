d6106557fd2afac937938fabe9e76c9d05e52e42
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2024-07-29 20:31:35 +0200

    Exported isProblem

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { check, checkAsync } from "./check.js";
-import { Err, Ok, Problem } from "./handling.js";
+import { Err, isProblem, Ok, Problem } from "./handling.js";
 import type { ExtractAsyncData, ExtractData, Result } from "./handling.js";
 import { mayFail, mayFailAsync } from "./mayFail.js";
 
-export { check, checkAsync, Err, mayFail, mayFailAsync, Ok, Problem };
+export {
+  check,
+  checkAsync,
+  Err,
+  isProblem,
+  mayFail,
+  mayFailAsync,
+  Ok,
+  Problem,
+};
 export type { ExtractAsyncData, ExtractData, Result };
