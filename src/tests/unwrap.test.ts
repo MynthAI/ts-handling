89c65c421317f4407b6eb76ec48c14064a01b656
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2025-02-14 12:59:01 +0100

    Don't export `Problem` (#5)

--- src/tests/unwrap.test.ts
+++ src/tests/unwrap.test.ts
@@ -1,5 +1,5 @@
 import invariant from "tests/invariant";
-import { Err, Ok, Problem, Result } from "ts-handling";
+import { Err, isProblem, Ok, Result } from "ts-handling";
 import { expect, test } from "vitest";
 
 const run = (succeed: boolean): Result<string, number> => {
@@ -8,12 +8,12 @@ const run = (succeed: boolean): Result<string, number> => {
 
 test("can unwrap fail", () => {
   const result = run(false).unwrap();
-  invariant(result instanceof Problem);
+  invariant(isProblem(result));
   expect(result.error).toBe(10);
 });
 
 test("can unwrap succeed", () => {
   const result = run(true).unwrap();
-  invariant(!(result instanceof Problem));
+  invariant(!isProblem(result));
   expect(result).toBe("success");
 });
